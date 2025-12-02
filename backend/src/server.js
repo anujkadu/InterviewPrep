b8513a0c4b5bd3ce68a3960ad86d5574c00a7079
anujkadu <kaduanuj605@gmail.com>
2025-12-02 12:41:55 +0530

    Fixed some errors and trying to connect Clerk with MongoDB using inngest

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,19 @@
 
 import express from "express";
 import path from "path";
+import cors from "cors";
 import {ENV} from "./lib/env.js"; //configure dotenv
+import {serve} from "inngest/express";
+import {inngest,functions} from "./lib/inngest.js";
 const app = express()     //initialise app
 
 const __dirname = path.resolve(); //to get current directory path
+
+
+app.use(express.json())  //middleware to parse json body
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest",serve({client:inngest, functions}))
 
 app.get("/health",(req,res)=>{
     res.status(200).json({msg:"API working"})
@@ -21,4 +30,22 @@ if(ENV.NODE_ENV === "development"){
     });
 }
 
-app.listen(ENV.PORT,()=> console.log("Server running on port:",ENV.PORT))
+
+const startServer = async()=>{
+    try{
+        await connectDB();
+        app.listen(ENV.PORT,()=> {
+        console.log("Server running on port:",ENV.PORT)
+
+    });
+    } 
+    catch(err){
+        console.log("Error starting server:",err)
+    }
+};
+
+startServer();
+
+
+
+//app.listen(ENV.PORT,()=> console.log("Server running on port:",ENV.PORT))
