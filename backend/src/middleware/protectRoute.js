6d03ec1abf34594ea108cd206f385c9f34d7e7cd
anujkadu <kaduanuj605@gmail.com>
2025-12-02 15:46:00 +0530

    Server side auth for Clerk, get token for stream implemented

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -6,17 +6,17 @@ export const protectRoute = [
     async (req,res,next)=>{
         try{
             const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(401).json({msg:"Unauthorized"});
+            if(!clerkId) return res.status(401).json({message:"Unauthorized"});
             //find user in db by clearkId
             const user = await User.findOne({clerkId})
-            if(!user) return res.status(404).json({msg:"User not found"});
+            if(!user) return res.status(404).json({message:"User not found"});
             // attach user to request object
             req.user = user
 
             next()
         } catch(err){
             console.error("Error in protectRoute middleware:",err);
-            res.status(500).json({msg:"Internal server error"});
+            res.status(500).json({message:"Internal server error"});
         }
     }
 ]
