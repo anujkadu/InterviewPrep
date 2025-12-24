2691145661af8c4a553914dd3d40f5eb6977b6c9
anujkadu <kaduanuj605@gmail.com>
2025-12-25 00:56:52 +0530

    Landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,19 @@ import { SignedOut, SignInButton, SignedIn, UserButton, SignOutButton , useUser}
 import { Route,Routes,Navigate } from 'react-router';
 import HomePage from './pages/HomePage.jsx';
 import {Toaster } from 'react-hot-toast'; 
-
+import DashboardPage from './pages/DashboardPage.jsx';
 import ProblemsPage from './pages/ProblemsPage.jsx';
 function App() {
-      const { isSignedIn }= useUser()
+      const { isSignedIn,isLoaded }= useUser();
+
+      //this gets rid of flicker effect
+      if(!isLoaded) return null;
   return (
     <>
     <Routes>  
       
-      <Route path="/" element ={<HomePage />} />
+      <Route path="/" element ={!isSignedIn ? <HomePage/> :<Navigate to={"/dashboard"}/>}/>
+      <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to = {"/"}/> }/>
     
       <Route path ="/problems" element={isSignedIn ?<ProblemsPage/> : <Navigate to={"/"}/>}/>
       </Routes>
